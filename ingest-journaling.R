2b31d62d173532df395aa7a023dcabcac3f03606
Paul Jones <pjones@ncsu.edu>
2015-03-28 18:20:52 -0400

    Tweaks to journaling ingest

--- ingest-journaling.R
+++ ingest-journaling.R
@@ -1,5 +1,7 @@
-# Function to ingest data from Journaling project
-# Paul Jones, pjones@ncsu.edu 3/25/15
+############################################################
+# CSC791 P3: Function to ingest data from Journaling project
+# Paul Jones, pjones@ncsu.edu, Last updated 3/25/2015
+############################################################
 
 #library(Rcurl)
 #library(TimeSeries)
@@ -17,7 +19,8 @@ ingest_journaling <- function(timestamp) {
   #           "data.EvtTime":{"$gte":1423717200000,"$lte":1424303540000}}}'
   #     https://las-skylr-token.oscar.ncsu.edu/api/data/document/query
   
-  # TO DO: Ingest JSON object
+  # TO DO: Insert timestamp into request
+
   resp <- POST("https://las-skylr-token.oscar.ncsu.edu/api/data/document/query",
                accept_json(),
                verbose(),
@@ -35,15 +38,23 @@ ingest_journaling <- function(timestamp) {
                  )
                ))
   )
+
+  # Download query content from server
   h<-content(resp, "text", encoding = "ISO-8859-1", col.names=FALSE, row.names=FALSE)
+  
+  # Parse JSON
   data.Journaling <- jsonlite::fromJSON(h)
+  
+  # Extract nested data object into data frame
   data.Journaling <- data.Journaling$data$data
+  
+  # For debugging only
   #head(data.Journaling)
   #dim(data.Journaling)
   #names(data.Journaling)
   #fix(data.Journaling)
-  # TO DO: Parse and extract timestamps
   
-  # TO DO: Create timeSeries object
+  # Just return the data frame (gets converted to a timeSeries object
+  # by the create_timeseries function in preprocess.R)
   return(data.Journaling) 
 }
