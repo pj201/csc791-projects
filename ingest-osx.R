944a6176d0bc1ec8a9c7e80a890db7aa90916766
kshitij1489 <kshitij.sharma.nits@gmail.com>
2015-04-02 14:39:16 -0400

    Added Function to ingest data from OSX Instruments project

---
+++ ingest-osx.R
@@ -0,0 +1,57 @@
+############################################################
+# CSC791 P3: Function to ingest data from Journaling project
+# Paul Jones, pjones@ncsu.edu, Last updated: 3/25/2015
+############################################################
+
+#library(Rcurl)
+#library(TimeSeries)
+library(jsonlite)
+library(httr)
+library(RJSONIO)
+
+# Returns an object containing a time series of CSC791 Journaling data since timestamp
+ingest_osx <- function(timestamp) {
+  
+  # Get latest data - equivalent curl command looks like:
+  # curl -H "Content-Type: application/json" 
+  #      -H "AuthToken: ${AUTH_TOKEN}" 
+  #      -d '{"type":"find","query":{"data.UserId”:”${UNITYID}","data.ProjId":"journaling-chrome",
+  #           "data.EvtTime":{"$gte":1423717200000,"$lte":1424303540000}}}'
+  #     https://las-skylr-token.oscar.ncsu.edu/api/data/document/query
+  
+  # TO DO: Insert timestamp into request
+
+  resp <- POST("https://las-skylr-token.oscar.ncsu.edu/api/data/document/query",
+              accept_json(),
+              verbose(),
+              add_headers("Content-Type" = "application/json", "AuthToken" = "79e0eb5ad4c0a4ceff6889dc6e8fd3ec188219db7a8a7e63d04a0730c2f9f0c9"),
+              body = toJSON(list(
+                "type" = "find",
+                "query" = list(
+                  "data.ProjId" = "skylr-osx-instrumenter",
+                  "data.UserId" = "pjones",
+                  "data.EvtTime" = list(
+                    "$gte" = 1423717200000,
+                    "$lte" = 1424303540000
+                  )
+                )
+              ))
+)
+
+  # Download query content from server
+  h<-content(resp, "text", encoding = "ISO-8859-1", col.names=FALSE, row.names=FALSE)
+  
+  # Parse JSON
+  data.OSX <- jsonlite::fromJSON(h)
+  
+  # Extract nested data object into data frame
+  data.OSX<-data.OSX$data$data
+  
+  # For debugging only
+  #head(data.OSX)
+  #data.OSX
+  
+  # Just return the data frame (gets converted to a timeSeries object
+  # by the create_timeseries function in preprocess.R)
+  return(data.Journaling) 
+}
