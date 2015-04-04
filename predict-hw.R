0c74a3ff6a59ff189509c25bcd48086e696538d9
Paul Jones <pjones@ncsu.edu>
2015-04-04 11:39:03 -0400

    Added new predict file

---
+++ predict-hw.R
@@ -0,0 +1,19 @@
+############################################################
+# CSC791 P3: Function to build and forecast based on
+# a Holt-Winters time series model.
+# Paul Jones, pjones@ncsu.edu, Last updated: 4/4/2015
+############################################################
+
+predict_hw <- function(timeseries) {
+
+# [PJ] Run Holt-Winters prediction (non-seasonal)
+fit_hw <- HoltWinters(timeseries, gamma=FALSE)
+
+# [PJ] Plot the Holt-Winters model
+plot(fit_hw)
+
+# [PJ] Plot forecasts from the model. TODO - explain more.
+plot(forecast(fit_hw))
+
+}
+
