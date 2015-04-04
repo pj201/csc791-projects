eb417d926710ddb1b76af72f0742e1f54ada649c
Preetham MS <me@preetham.in>
2015-04-04 13:25:22 -0400

    Adding install script
    Signed-off-by: Preetham MS <me@preetham.in>

---
+++ install.R
@@ -0,0 +1,10 @@
+############################################################
+# CSC791 P3: R Script to install all the packages which are required for P3
+# Preetham, pmahish@ncsu.edu, Last updated: 4/4/2015
+############################################################
+
+install.packages("twitteR")
+install.packages("forecast")
+install.packages("jsonlite")
+install.packages("httr")
+install.packages("RJSONIO")
