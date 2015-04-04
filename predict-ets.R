9766593551c98fb9941f78bae91be5ef921f8c01
Nitin Tak <ntak@ncsu.edu>
2015-04-04 14:19:15 -0400

    Exponential smoothign state space model

---
+++ predict-ets.R
@@ -0,0 +1,19 @@
+############################################################
+# CSC791 P3: Function to build and forecast based on
+# a Exponential smoothing state space model ()ets)
+# Nitin Tak, ntak@ncsu.edu
+############################################################
+
+predict_ets <- function(timeseries) {
+
+# [NT] Run ets prediction (non-seasonal)
+fit_ets <- ets(timeseries, gamma=FALSE)
+
+# [NT] Plot the Holt-Winters model
+#plot(fit_ets)
+
+# [NT] Plot forecasts from the model
+plot(forecast(fit_ets))
+
+}
+
