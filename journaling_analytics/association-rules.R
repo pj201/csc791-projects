aa890b718d01fc576fd84ab55cbacaf15a608492
kshitij <kshitj_1489>
2015-04-24 23:52:14 -0400

    add code to extract association rules

--- journaling_analytics/association-rules.R
+++ journaling_analytics/association-rules.R
@@ -1,43 +1,80 @@
+############################################################
+# CSC791 P3: Function to extract rules using Apriori from
+# Journaling data set. 
+# Paul Jones, pjones@ncsu.edu, Last updated: 4/24/2015
+############################################################
 
-install.packages("arules")
+#install.packages("arules")
 library("arules")
-install.packages("arulesViz")
+#install.packages("arulesViz")
 library("arulesViz")
 
-new_j_df
+# extracting rules using Apriori Algorithm from 
+# Journaling Dataset.
+extract_rules <- function(new_j_df) {
 
+# Purging out other attributes except for the UserId, EvtTime, WebURL.
 colname<- c("UserId", "EvtTime", "WebURL")
 subsetJournaling<- new_j_df[colname]
 
+# Setting a global variable, a counter which indicates the previous Transaction ID. 
 currentTransId <<- 1
 
-
-
+#In the next three lines, we are creating a new column NextEvtTime, which 
+# which holds the EvtTime of the next Event.
 NextEvtTime <- subsetJournaling$EvtTime[2:nrow(subsetJournaling)] 
 NextEvtTime[nrow(subsetJournaling)]<- NextEvtTime[nrow(subsetJournaling)-1]
 subsetJournaling$NextEvtTime<-NextEvtTime
 
+# setting the maximum inactivity time gap allowed for a URL to be a 
+# part of the transaction.  
+inActiv <- 3600000
+
+# Function which returns a vector of Transaction Id corresponding to each
+# WebURL.
 compareRows<-function(x) {
-  
-  if(as.numeric(x[4]) - as.numeric(x[2]) > 3600000) {
+
+# If there is no activity for a period of inActiv duration, 
+# we create a new transaction.
+  if(as.numeric(x[4]) - as.numeric(x[2]) > inActiv) {
     currentTransId<<-currentTransId+1; 
   }
   return(currentTransId)
 }
 
+# We club all the web URL's into one transaction if there is no 
+# inactivity duration greater than inActiv, else we start a new 
+# transactionId.  
 transIds<-apply(subsetJournaling, 1, compareRows)
 
+# Adding a new column TransId which corresponds to the transaction ID.
 subsetJournaling$TransId <- transIds
 
+# Subsetting TransId and WebURL from the subsetJournaling data frame.
 transJourn<-subsetJournaling[c("TransId","WebURL")]
+
+# Removing Duplicate WebURL's from each transaction.
 ddup <- unique(transJourn)
+
+# Writing a transJourn.csv to the current dir location.
 write.csv(ddup, file = "transJourn.csv", row.names = FALSE)
 
+# Creating a transaction object from the transJourn.csv
 trans <- read.transactions("transJourn.csv" , format = c("single"), sep = ',',
                            cols = c(1,2))
+# Using Apriori Algorithm to select only those rules whose support and confidence satisfy
+# the specified threshhold in the function.						   
 rules <- apriori(trans, parameter = list(supp = 0.1, conf = 0.8))
+
+# Plotting a Support vs Confidence graph for the rules which satisfy the support and confidence 
+# threshhold.
+png("../journaling_dashboard/public/images/Support-Confidence.png")
 plot(rules, control=list(jitter=2))
+dev.off()
+
 rules_high_lift <- head(sort(rules, by="lift"), 3)
 inspect(rules_high_lift)
+
 plot(rules_high_lift, method="graph", control=list(type="items"))
 
+}
--- journaling_analytics/main.R
+++ journaling_analytics/main.R
@@ -1,7 +1,7 @@
 ############################################################
 # CSC791 P3: Main function to demonstrate streaming
 # time series analysis techniques in R.
-# Nitin,Paul,Kshitij,Denil,Preetham. Last updated: 4/5/2015
+# Nitin,Paul,Kshitij,Dakota Last updated: 4/24/2015
 ############################################################
 
 ############################################################
@@ -21,13 +21,12 @@ setwd(this.dir)
 
 # Load functions from other files
 source("ingest-journaling.R")
-source("ingest-osx.R")
-source("ingest-twitter.R")
 source("preprocess.R")
 source("predict-ets.R")
 source("predict-arima.R")
 source("predict-garch.R")
 source("predict-hw.R")
+source("association-rules.R")
 
 # Sleep function (used by streaming ingest loop)
 sleep <- function(x)
@@ -52,24 +51,13 @@ cat("**************************************************************\n")
 cat("* Welcome to a demo of time series analysis techniques in R! *\n")
 cat("**************************************************************\n\n")
 
-cat("Press enter to fetch historical data from the LAS OSXInstrumenter...")
-t<-readline()
-
-# Fetch Data from OSX Instrumenter
-new_osx_df <- ingest_osx(previousTime, presentTime)
-
 cat("Press enter to fetch historical data from the CSC791 Journaling Trial...")
 t<-readline()
 
 # Fetch Data from Journaling project
 new_j_df <- ingest_journaling(previousTime, presentTime)
-
-cat("Press enter to fetch historical data from Twitter (@BarackObama)...")
-cat("\n[Make sure you've added your Twitter Oauth credentials to ingest-twitter.R]")
-t<-readline()
-
-# Fetch Data from Twitter
-new_t_df <- ingest_twitter("BarackObama",200) 
+# Extract rules from Journaling project
+extract_rules(new_j_df)
 
 # Initialise timeseries object
 tsOld <- ts(c(0));
@@ -85,26 +73,8 @@ cat("Press enter to create time series objects from the 3 datasets...")
 t<-readline()
 
 history_j_ts <- create_timeseries(new_j_df, tsNull, "UserId", "EvtTime", size)
-history_osx_ts <- create_timeseries(new_osx_df, tsNull, "UserId", "EvtTime", size)
-history_twitter_ts <- create_timeseries(new_t_df, tsNull, "id", "EvtTime", size)
 
 # Run forecasting algorithms on historic data
-
-cat("Press enter to build prediction models on LAS OSXInstrumenter activity data...")
-t<-readline()
-
-par(mfrow=c(2,2))
-
-cat("Building Holt-Winters model...\n")
-predict_hw(history_osx_ts, label="OSXInstrumenter")
-cat("Building GARCH model...\n")
-predict_garch(history_osx_ts, label="OSXInstrumenter")
-cat("Building ARIMA model...\n")
-predict_arima(history_osx_ts, label="OSXInstrumenter")
-cat("Building Exponential Smoothing model...\n")
-predict_ets(history_osx_ts, label="OSXInstrumenter")
-
-mtext("Time Series Forecasts for LAS OSXInstrumenter (pjones) Dataset",outer=TRUE,line=-1.5)
 
 cat("\nPress enter to build prediction models on CSC791 Journaling activity data...")
 t<-readline()
@@ -122,22 +92,6 @@ predict_ets(history_j_ts, label="Journaling")
 
 mtext("Time Series Forecasts for LAS Journaling Dataset",outer=TRUE,line=-1.5)
 
-cat("\nPress enter to build prediction models on @BarackObama's twitter activity data...")
-t<-readline()
-
-par(mfrow=c(2,2))
-
-cat("Building Holt-Winters model...\n")
-predict_hw(history_twitter_ts, label="@BarackObama's twitter")
-cat("Building GARCH model...\n")
-predict_garch(history_twitter_ts, label="@BarackObama's twitter")
-cat("Building ARIMA model...\n")
-predict_arima(history_twitter_ts, label="@BarackObama's twitter")
-cat("Building Exponential Smoothing model...\n")
-predict_ets(history_twitter_ts, label="@BarackObama's twitter")
-
-mtext("Time Series Forecasts for Twitter (@BarackObama) Dataset",outer=TRUE,line=-1.5)
-
 ############################################################
 # Ingest continuous streaming data and make forecasts
 ############################################################
@@ -154,37 +108,16 @@ t<-readline()
 
   # Get new data for each time series
   
-  cat("Getting new data from LAS OSXInstrumenter project...")
-  new_osx_df <- ingest_osx(previousTime, presentTime)
   cat("Getting new data from CSC791 Journaling Trial...")
   new_j_df <- ingest_journaling(previousTime, presentTime) 
-  cat("Getting new data from Twitter (@BarackObama)...")
-  new_t_df <- ingest_twitter("BarackObama",200)
-
+  
   cat("\n\nUpdating time series...\n")
   # If we have new data, pre-process to create time series
   if(!is.null(new_j_df)) {
   history_j_ts <- create_timeseries(new_j_df, history_j_ts, "UserId", "EvtTime", size)
   }
-  if(!is.null(new_osx_df)) {
-  history_osx_ts <- create_timeseries(new_osx_df, history_osx_ts, "UserId", "EvtTime", size)
-  }
-  history_twitter_ts <- create_timeseries(new_t_df, tsNull, "id", "EvtTime", 720)
 
   # Run forecasting algorithms to update for next time period
-
-  cat("\nUpdating prediction models for LAS OSXInstrumenter activity data...\n")
-  par(mfrow=c(2,2))
-  cat("Updating Holt-Winters model...\n")
-  predict_hw(history_osx_ts, label="OSXInstrumenter")
-  cat("Updating GARCH model...\n")
-  predict_garch(history_osx_ts, label="OSXInstrumenter")
-  cat("Updating ARIMA model...\n")
-  predict_arima(history_osx_ts, label="OSXInstrumenter")
-  cat("Updating Exponential Smoothing model...\n")
-  predict_ets(history_osx_ts, label="OSXInstrumenter")
-  
-  mtext("Time Series Forecasts for LAS OSXInstrumenter (pjones) Dataset",outer=TRUE,line=-1.5)
 
   cat("\nUpdating prediction models for CSC791 Journaling activity data...\n")
   par(mfrow=c(2,2))
@@ -199,24 +132,10 @@ t<-readline()
 
   mtext("Time Series Forecasts for CSC791 Journaling Activity Dataset",outer=TRUE,line=-1.5)
 
-  # TO DO: Update models for Twitter!
-
-  cat("\nUpdating prediction models for @BarackObama'a twitter activity data...\n")
-  par(mfrow=c(2,2))
-  cat("Updating Holt-Winters model...\n")
-  predict_hw(history_twitter_ts, label="@BarackObama's twitter")
-  cat("Updating GARCH model...\n")
-  predict_garch(history_twitter_ts, label="@BarackObama's twitter")
-  cat("Updating ARIMA model...\n")
-  predict_arima(history_twitter_ts, label="@BarackObama's twitter")
-  cat("Updating Exponential Smoothing model...\n")
-  predict_ets(history_twitter_ts, label="@BarackObama's twitter")
-  
-  mtext("Time Series Forecasts for Twitter (@BarackObama) Dataset",outer=TRUE,line=-1.5)
-
   # Wait one minute betwen data requests 
   # (should be one hour in a real app)
   #sleep(60)
   
   # End of continuous ingest loop
 #}
+
