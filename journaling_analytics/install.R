369980e5e0674c8d1030af9da7977555e7033e75
Paul Jones <pjones@ncsu.edu>
2015-05-04 15:46:47 -0400

    tweaked instal.R

--- journaling_analytics/install.R
+++ journaling_analytics/install.R
@@ -1,11 +1,9 @@
 ############################################################
-# CSC791 P3: R Script to install all the packages which are required for P3
-# Preetham, pmahish@ncsu.edu, Last updated: 4/4/2015
+# CSC791 P5: R Script to install all the required pkgs
+# Paul Jones, Last updated: 5/4/2015
 ############################################################
 
-install.packages("twitteR")
 install.packages("forecast")
 install.packages("jsonlite")
 install.packages("httr")
 install.packages("RJSONIO")
-install.packages("fGarch")
